eff89334f907f38cc1d7bcb3a12e262aee93b6e7
Carolina Guida <carolinaguidaa@gmail.com>
2021-05-06 16:18:05 -0300

    Create Starbucks Landing Page

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+function imgSlider1(){
+    document.querySelector('.starbucks').src = "https://i.imgur.com/LRffwt5.png";
+}
+function imgSlider2(){
+    document.querySelector('.starbucks').src = "https://i.imgur.com/1G1J2v2.png";
+}
+function imgSlider3(){
+    document.querySelector('.starbucks').src = "https://i.imgur.com/LUU1SXt.png";
+}
+
+
+
+function changeBgColor(color){
+    const bg = document.querySelector('.bg-1');    
+    bg.style.backgroundImage = color;
+    
+} 
+
